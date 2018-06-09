63ee8d0816ea130060f67129dffab402f2ec54ca
tvio <tvio@centrum.cz>
2018-06-09 20:22:59 +0200

    local and remote port

--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ const express = require ('express');
 const app = express();
 const publicPath = path.join(__dirname,'../public');
 
+var port = process.env.PORT || 3000;
+
 app.use(express.static(publicPath));
 app.get('/',(req,res)=>res.send('Ahojky'));
-app.listen(3000,()=>console.log('Chat app bezi na portu 3000'));
+app.listen(port,()=>console.log(`Chat app bezi na portu ${port}`));
 
 
 
-console.log(__dirname+ '/../public');
-console.log(publicPath);
+// console.log(__dirname+ '/../public');
+// console.log(publicPath);
