a91ba88ef6d6d1f501dbb3372c44a02e932a6c25
tvio <tvio@centrum.cz>
2018-06-09 21:43:13 +0200

    pridan socket.io plus  connect a disconnect

--- server/server.js
+++ server/server.js
@@ -1,13 +1,33 @@
 const path = require('path');
+const http = require('http');
 const express = require ('express');
-const app = express();
+
+const socketIO = require('socket.io');
 const publicPath = path.join(__dirname,'../public');
 
-var port = process.env.PORT || 3000;
+var app = express();
+var server = http.createServer(app);
+var io = socketIO(server);
+
+const port = process.env.PORT || 3000;
 
 app.use(express.static(publicPath));
+
+
 app.get('/',(req,res)=>res.send('Ahojky'));
-app.listen(port,()=>console.log(`Chat app bezi na portu ${port}`));
+
+
+io.on('connection',(socket)=>{
+    console.log('New User connected');
+
+    socket.on('disconnect',()=>{
+        console.log('User was disconnected');
+    });
+    
+});
+
+
+server.listen(port,()=>console.log(`Chat app bezi na portu ${port}`));
 
 
 
