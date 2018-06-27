afb8270da6978c15bac720992a1a63f4589afe0d
Hájek Tomáš <hajek@sukl.cz>
2018-06-27 11:39:37 +0200

    Scroll to bottom

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,23 @@
 
 var socket =  io();
+
+function scrollToBottom (){
+    // Selectors
+    var messages = jQuery('#messages');
+    var newMessage = messages.children('li:last-child');
+    //Heights
+    var clientHeight = messages.prop('clientHeight');
+    var scrollTop = messages.prop('scrollTop');
+    var scrollHeight = messages.prop('scrollHeight');
+    var newMessageHeight = newMessage.innerHeight();
+    var lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (clientHeight + scrollTop + newMessageHeight + lastMessageHeight <= scrollHeight) {
+        //console.log('Should scroll');
+        messages.scrollTop(scrollHeight);
+    }
+
+};
 // arrow funkce aokrat v chrome
 socket.on('connect', function(){
     console.log('connected to server');
@@ -22,6 +40,7 @@ socket.on('newMessage', function(message){
     });
 
     jQuery('#messages').append(html);
+    scrollToBottom();
 
 
 // var li = jQuery('<li></li>');
@@ -49,7 +68,7 @@ socket.on('newLocationMessage',function(message){
     });
 
     jQuery('#messages').append(html);
-
+    scrollToBottom();
 
 
     //   var li = jQuery('<li></li>');
